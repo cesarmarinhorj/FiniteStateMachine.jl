e53648ebcb5f479cc645134f47e2be0d9bb607c7
Cesar Marinho <cesarmarinhorj@gmail.com>
2022-12-14 22:30:13 -0300

    correção de erros: . não rodava o teste . struct estava immutable

--- src/FiniteStateMachine.jl
+++ src/FiniteStateMachine.jl
@@ -3,7 +3,7 @@ module FiniteStateMachine
 
 export StateMachine, state_machine, fire, callback, before_event, before_this_event, before_any_event, after_event, after_this_event, after_any_event, change_state, enter_state, enter_this_state, enter_any_state, leave_state, leave_this_state, leave_any_state
 
-struct StateMachine
+mutable struct StateMachine
     map::Dict
     actions::Dict
     current::AbstractString
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using FiniteStateMachine
-using Base.Test
+using Test
 
 include("test_state_machine.jl")
 
